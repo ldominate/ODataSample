1d9ec24ad749b34df26c77d413e2243bc63cf877
Евгений <loki_jh@bk.ru>
2015-11-14 21:23:44 +1000

    Сам пример

---
+++ ODataSample/App_Start/WebApiConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using Escowt.Domain.Globalization;
+
+namespace ODataSample
+{
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			// Раскомментируйте следующую строку кода, чтобы включить поддержку запросов для действий с типом возвращаемого значения IQueryable или IQueryable<T>.
+			// Чтобы избежать обработки неожиданных или вредоносных запросов, используйте параметры проверки в QueryableAttribute, чтобы проверять входящие запросы.
+			// Дополнительные сведения см. по адресу http://go.microsoft.com/fwlink/?LinkId=279712.
+			config.EnableQuerySupport();
+
+			ODataModelBuilder modelBuilder = new ODataConventionModelBuilder();
+
+			modelBuilder.EntitySet<Language>("Language");
+
+			Microsoft.Data.Edm.IEdmModel model = modelBuilder.GetEdmModel();
+			config.Routes.MapODataRoute("ODataRoute", "odata", model);
+
+			// Чтобы отключить трассировку в приложении, закомментируйте или удалите следующую строку кода
+			// Дополнительные сведения см. по адресу: http://www.asp.net/web-api
+			config.EnableSystemDiagnosticsTracing();
+
+		}
+	}
+}
---
+++ ODataSample/Controllers/LanguageController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.OData;
+using Escowt.Data;
+using Escowt.Domain.Globalization;
+using Microsoft.Data.OData;
+
+namespace ODataSample.Controllers
+{
+	public class LanguageController : EntitySetController<Language, Guid>
+    {
+		EscowtDB _escowtDB;
+
+		public LanguageController()
+		{
+			_escowtDB = new EscowtDB(ConfigurationManager.ConnectionStrings["ConnectionDB"].ToString());
+		}
+
+		public override IQueryable<Language> Get()
+		{
+			return _escowtDB.Languages;
+		}
+
+		protected override Language GetEntityByKey(Guid key)
+		{
+			return _escowtDB.Languages.FirstOrDefault(l => l.Guid == key);
+		}
+
+		protected override Language CreateEntity(Language language)
+		{
+			if (ModelState.IsValid)
+			{
+				language.Guid = Guid.NewGuid();
+				_escowtDB.Languages.Add(language);
+				_escowtDB.SaveChanges();
+				return language;
+			}
+			else
+			{
+				var response = Request.CreateResponse(HttpStatusCode.BadRequest, new ODataError
+				{
+					ErrorCode = "ValidationError",
+					Message = string.Join(";", ModelState.Values.First().Errors.Select(e => e.ErrorMessage).ToArray())
+
+				});
+				throw new HttpResponseException(response);
+			}
+		}
+
+		protected override Language UpdateEntity(Guid key, Language language)
+		{
+			if (!_escowtDB.Languages.Any(l => l.Guid == key))
+			{
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+			}
+			_escowtDB.Languages.Attach(language);
+			_escowtDB.Entry(language).State = EntityState.Modified;
+			_escowtDB.SaveChanges();
+			return language;
+		}
+
+		protected override Language PatchEntity(Guid key, Delta<Language> patch)
+		{
+			var language = _escowtDB.Languages.FirstOrDefault(l => l.Guid == key);
+
+			if (language == null)
+			{
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+			}
+			patch.Patch(language);
+			_escowtDB.Languages.Attach(language);
+			_escowtDB.SaveChanges();
+			return language;
+		}
+
+		public override void Delete([FromODataUri]Guid key)
+		{
+			var language = _escowtDB.Languages.FirstOrDefault(l => l.Guid == key);
+
+			if (language == null)
+			{
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+			}
+			_escowtDB.Languages.Remove(language);
+			_escowtDB.SaveChanges();
+		}
+
+		protected override Guid GetKey(Language language)
+		{
+			return language.Guid;
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_escowtDB.Dispose();
+			base.Dispose(disposing);
+		}
+    }
+}
